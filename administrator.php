dc0c6ea452005d36cf7a4f37a22552c39f416c41
septem2tmac <septem2tmac@gmail.com>
2016-09-12 20:55:05 -0500

    first commit

--- administrator.php
+++ administrator.php
@@ -2,22 +2,22 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-    <title>Payment</title>
+	<title>Payment</title>
 
 	<link id="switch_style" href="bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="css/theme.css" rel="stylesheet">
+	<link href="css/theme.css" rel="stylesheet">
 	
 	<script src="js/courseselection.js"></script>
 </head>
 
 <body>
-    <div class="row directory">
+	<div class="row directory">
 		<div class="col-sm-12 ">
 			<h2><span>Course listings</span></h2>
 		</div>
